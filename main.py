b24ce6d1ebdf64a91773da4075d7c0970557b607
justin <jtr342@gmail.com>
2023-01-23 22:27:03 -0800

    updated pipeline to insert into DB

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ def main(params):
     df =  pd.read_csv('taxi+_zone_lookup.csv', nrows=100)
     ##table_name = params.table_name
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    engine.connect()
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
 
