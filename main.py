5b429d1b54875083e3d6c43bfac02ac796f6ca82
justin <jtr342@gmail.com>
2023-01-24 19:15:35 -0800

    troubleshooting

--- main.py
+++ main.py
@@ -15,19 +15,18 @@ def main(params):
     host = params.host 
     port = params.port 
     db = 'ny_taxi'
-    df =  pd.read_csv('taxi+_zone_lookup.csv', nrows=100)
     ##table_name = params.table_name
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df_iter = pd.read_csv('taxi+_zone_lookup.csv', nrows=100)
+    print(df_iter.to_string()) 
+    ##df = next(df_iter)
 
-    df = next(df_iter)
+    # df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    # df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+    # df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
 
 
 
